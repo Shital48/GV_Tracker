76711500217d19b69291fb392dbbe02f09c10764
Shital Gadekar <shitalgadekar48@gmail.com>
2024-11-08 06:29:44 +0000

    Bill Details

--- webapp/controller/BillDetails.controller.js
+++ webapp/controller/BillDetails.controller.js
@@ -1,45 +1,77 @@
-   sap.ui.define([
+sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/m/Image"
-], function (Controller, MessageToast, Image) {
+    "sap/ui/model/json/JSONModel",
+    "sap/m/Table",
+    "sap/m/Button",
+    "sap/m/Image",
+    "sap/m/HBox"
+  ], function (Controller, JSONModel, Table, Button, Image, HBox) {
     "use strict";
 
     return Controller.extend("gvtracker.controller.BillDetails", {
 
-       // Handle file selection and display the image
-       onFileChange: function (oEvent) {
-        var oFiles = oEvent.getParameter("files");
-
-        // Process each selected file
-        for (var i = 0; i < oFiles.length; i++) {
-            var oFile = oFiles[i];
-
-            // Only process image files
-            if (oFile && oFile.type.startsWith("image/")) {
-                var oReader = new FileReader();
-                oReader.onload = function (e) {
-                    // Create a new Image control for each uploaded image
-                    var oImage = new Image({
-                        src: e.target.result,
-                        width: "100px",
-                        height: "100px" 
-                    });
-                     // Apply the CSS class to the image control
-                    oImage.addStyleClass("photoThumbnail");
-                    // Add the image to the photo display lane (HBox)
-                    this.getView().byId("photoDisplayLane").addItem(oImage);
-                }.bind(this);
-
-                // Read the image file as a data URL
-                oReader.readAsDataURL(oFile);
-            } else {
-                MessageToast.show("Please select a valid image file.");
+        onInit: function () {
+            var oModel = new JSONModel({
+              records: [
+                { customerBillNo: "Bill-001", photoPath: "" },
+                { customerBillNo: "Bill-002", photoPath: "" },
+                { customerBillNo: "Bill-003", photoPath: "" },
+                { customerBillNo: "Bill-004", photoPath: "" }
+              ],
+              images: ["", "", "", ""]
+            });
+            this.getView().setModel(oModel);
+          },
+      
+          onFileUploadChange: function (oEvent) {
+            var oFileUploader = oEvent.getSource();
+            var oModel = this.getView().getModel();
+          
+            // Retrieve the file
+            var oFile = oEvent.getParameter("files")[0];
+            var reader = new FileReader();
+            reader.onload = function (e) {
+              // Store the file as an image in the first empty slot
+              var aImages = oModel.getProperty("/images");
+              for (var i = 0; i < aImages.length; i++) {
+                if (!aImages[i]) {
+                  aImages[i] = e.target.result;
+                  break;
+                }
+              }
+              oModel.setProperty("/images", aImages);
+            }.bind(this);
+          
+            reader.readAsDataURL(oFile);
+          },
+          
+          onDeleteRow: function (oEvent) {
+            var oButton = oEvent.getSource();
+            var oItem = oButton.getParent();
+            var oTable = this.byId("billDetailsTable");
+            var oContext = oItem.getBindingContext();
+            var sPath = oContext.getPath();
+            var oModel = this.getView().getModel();
+      
+            // Remove the record
+            var aRecords = oModel.getProperty("/records");
+            var index = aRecords.findIndex(function (record) {
+              return record.customerBillNo === oModel.getProperty(sPath).customerBillNo;
+            });
+      
+            // If record is found, delete it
+            if (index > -1) {
+              aRecords.splice(index, 1);
+              oModel.setProperty("/records", aRecords);
             }
-        }
-
-        // Clear the FileUploader after use
-        this.getView().byId("fileUploader").clear();
-    }
+          },
+      
+          onAddRow: function () {
+            var oModel = this.getView().getModel();
+            var aRecords = oModel.getProperty("/records");
+            var newRow = { customerBillNo: "New Bill", photoPath: "" };
+            aRecords.push(newRow);
+            oModel.setProperty("/records", aRecords);
+          }
     });
 });
